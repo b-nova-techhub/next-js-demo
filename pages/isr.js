e88fdf7bb7dce1f357ca645592c3b5f90d053b36
ttrapp <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>
2022-01-07 15:19:17 +0100

    isr

--- pages/isr.js
+++ pages/isr.js
@@ -1,29 +1,27 @@
-function ToDosIsr({ todos,now }) {
+function ToDosIsr({todos, now}) {
     return (
-      <><b>{now}</b><ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul></>
+        <><b>{now}</b>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.title}</li>
+                ))}
+            </ul>
+        </>
     )
-  }
-  
-  // This function gets called at build time
+}
+
+// This function gets called at build and request time
 export async function getStaticProps() {
-    // Call an external API endpoint to get posts
     const res = await fetch('https://jsonplaceholder.typicode.com/todos')
     const todos = await res.json()
     const now = Date.now()
-  
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
     return {
-      props: {
-        todos,
-        now,
-      },
-      revalidate: 10
+        props: {
+            todos,
+            now,
+        },
+        revalidate: 10
     }
-  }
+}
 
-  export default ToDosIsr
+export default ToDosIsr
