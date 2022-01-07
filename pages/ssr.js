d5024ede72609552d60a5d8d8f11176e8d81431d
ttrapp <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>
2022-01-07 14:10:28 +0100

    ssr

--- pages/ssr.js
+++ pages/ssr.js
@@ -1,26 +1,22 @@
-function ToDos({ todos }) {
+function ToDos({todos}) {
     return (
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-          ))}
-      </ul>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>{todo.title}</li>
+            ))}
+        </ul>
     )
-  }
-  
-  // This function gets called at build time
+}
+
+// This function gets called at request time
 export async function getServerSideProps() {
-    // Call an external API endpoint to get posts
     const res = await fetch('https://jsonplaceholder.typicode.com/todos')
     const todos = await res.json()
-  
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
     return {
-      props: {
-        todos,
-      },
+        props: {
+            todos,
+        },
     }
-  }
+}
 
-  export default ToDos
+export default ToDos
